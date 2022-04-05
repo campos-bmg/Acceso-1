0910b643d8ef432db8922ee6675eed07aec565b9
campos-bmg <91045702+campos-bmg@users.noreply.github.com>
2022-04-05 03:37:04 -0500

    DIseño base de interfaz, trabajo en progreso

--- Acceso/Form1.Designer.cs
+++ Acceso/Form1.Designer.cs
@@ -30,83 +30,133 @@
         {
             this.imgAlumno = new System.Windows.Forms.PictureBox();
             this.lblNombre = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblCarrera = new System.Windows.Forms.Label();
+            this.lblSemestre = new System.Windows.Forms.Label();
+            this.lblModalidad = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblMatricula = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.imgAlumno)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // imgAlumno
             // 
-            this.imgAlumno.Location = new System.Drawing.Point(296, 34);
+            this.imgAlumno.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.imgAlumno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(62)))), ((int)(((byte)(89)))));
+            this.imgAlumno.Location = new System.Drawing.Point(270, 4);
+            this.imgAlumno.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.imgAlumno.Name = "imgAlumno";
-            this.imgAlumno.Size = new System.Drawing.Size(219, 248);
+            this.imgAlumno.Size = new System.Drawing.Size(350, 395);
             this.imgAlumno.TabIndex = 0;
             this.imgAlumno.TabStop = false;
             // 
             // lblNombre
             // 
+            this.lblNombre.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(296, 300);
+            this.lblNombre.Font = new System.Drawing.Font("Oswald Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNombre.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblNombre.Location = new System.Drawing.Point(270, 414);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(38, 15);
+            this.lblNombre.Size = new System.Drawing.Size(350, 35);
             this.lblNombre.TabIndex = 1;
-            this.lblNombre.Text = "label1";
+            this.lblNombre.Text = "José Pérez Lopez";
+            this.lblNombre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label2
+            // lblCarrera
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(296, 323);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.lblCarrera.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCarrera.AutoSize = true;
+            this.lblCarrera.Location = new System.Drawing.Point(270, 464);
+            this.lblCarrera.Name = "lblCarrera";
+            this.lblCarrera.Size = new System.Drawing.Size(350, 20);
+            this.lblCarrera.TabIndex = 2;
+            this.lblCarrera.Text = "label2";
             // 
-            // label3
+            // lblSemestre
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(296, 346);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.lblSemestre.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSemestre.AutoSize = true;
+            this.lblSemestre.Location = new System.Drawing.Point(270, 492);
+            this.lblSemestre.Name = "lblSemestre";
+            this.lblSemestre.Size = new System.Drawing.Size(350, 20);
+            this.lblSemestre.TabIndex = 3;
+            this.lblSemestre.Text = "label3";
             // 
-            // label4
+            // lblModalidad
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(296, 369);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "label4";
+            this.lblModalidad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblModalidad.AutoSize = true;
+            this.lblModalidad.Location = new System.Drawing.Point(270, 520);
+            this.lblModalidad.Name = "lblModalidad";
+            this.lblModalidad.Size = new System.Drawing.Size(350, 20);
+            this.lblModalidad.TabIndex = 4;
+            this.lblModalidad.Text = "label4";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(296, 405);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(801, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 5;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Controls.Add(this.imgAlumno, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblModalidad, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblSemestre, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblNombre, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblCarrera, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblMatricula, 1, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(890, 576);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // lblMatricula
+            // 
+            this.lblMatricula.AutoSize = true;
+            this.lblMatricula.Location = new System.Drawing.Point(270, 544);
+            this.lblMatricula.Name = "lblMatricula";
+            this.lblMatricula.Size = new System.Drawing.Size(50, 20);
+            this.lblMatricula.TabIndex = 6;
+            this.lblMatricula.Text = "label1";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblNombre);
-            this.Controls.Add(this.imgAlumno);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(37)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imgAlumno)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -114,9 +164,11 @@
 
         private PictureBox imgAlumno;
         private Label lblNombre;
-        private Label label2;
-        private Label label3;
-        private Label label4;
+        private Label lblCarrera;
+        private Label lblSemestre;
+        private Label lblModalidad;
         private Button button1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label lblMatricula;
     }
 }
--- Acceso/Form1.cs
+++ Acceso/Form1.cs
@@ -1,18 +1,32 @@
-
-{
-
-}
 namespace Acceso
 {
     public partial class Form1 : Form
     {
+        public string defaultText = "Preparado para escanear...", emptyText = string.Empty;
         public Form1()
         {
             InitializeComponent();
+            lblNombre.Text = defaultText;
+            borrarTextos();
+            
         }
-
+        public void borrarTextos()
+        {
+            lblCarrera.Text = string.Empty;
+            lblSemestre.Text = string.Empty;
+            lblModalidad.Text = string.Empty;
+            lblMatricula.Text = string.Empty;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            Rectangle r = new Rectangle(0, 0, imgAlumno.Width, imgAlumno.Height);
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            int d = 21;
+            gp.AddArc(r.X, r.Y, d, d, 180, 90);
+            gp.AddArc(r.X + r.Width - d, r.Y, d, d, 270, 90);
+            gp.AddArc(r.X + r.Width - d, r.Y + r.Height - d, d, d, 0, 90);
+            gp.AddArc(r.X, r.Y + r.Height - d, d, d, 90, 90);
+            imgAlumno.Region = new Region(gp);
         }
     }
 }
